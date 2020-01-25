2b1cb66686bfb1e992fd67abdc425943481d5b70
manish436 <manish436@gmail.com>
2020-01-25 13:25:13 +0530

    Operand Promotion operation

--- src/main/java/com/hcl/learn/VariablesOpertorsArrays/Operators.java
+++ src/main/java/com/hcl/learn/VariablesOpertorsArrays/Operators.java
@@ -8,7 +8,10 @@ package com.hcl.learn.VariablesOpertorsArrays;
 // Rule 3. Can use parenthesis to change the evaluation order
 
 // Operand Promotion
-// Rule 1. 
+// Rule 1. Before any operation (addition or substractor or any other) variable will be promoted and then operation will be performed. 
+// like: byte + byte + int then these bytes will be promoted to int first before operation.
+// like: 'a' + 'b' so char will be promoted to int 97+98 so final result will be 195.
+
 public class Operators {
 	static void preAndPost() {
 		System.out.println("\nInside preAndPost ...");
@@ -53,6 +56,17 @@ public class Operators {
 
 	static void isOddOrEven(int num) {
 		System.out.println(num % 2);
+	}
+
+	// Operand Promotion operation
+	static void charTypePromotion() {
+		System.out.println("\nInside charTypePromotion ...");
+		char char1 = 50; // Will be assigned corresponding UTF16 value 2
+		System.out.println("char1: " + char1);
+		System.out.println("(73 - char1): " + (73 - char1)); // char1 gets promoted to int, i.e., decimal equivalent 50
+																// in UTF16 is used
+		System.out.println("(char1 - '3'): " + (char1 - '3')); // char1 & '3' are promoted to ints
+		System.out.println("('a' + 'b'): " + ('a' + 'b')); // 'a' & 'b' are promoted to ints and the respective
 	}
 
 	public static void main(String[] args) {
