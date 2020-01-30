55fe5e2bbb13d517ddb247de2cb929a2e42d75c8
manish436 <manish436@gmail.com>
2020-01-30 18:04:01 +0530

    Best practice and coding conventions

---
+++ src/main/java/com/hcl/learn/codingConvention/CodingConvention.java
@@ -0,0 +1,25 @@
+package com.hcl.learn.codingConvention;
+
+// Item 56 - Adhere to generally accepted naming conventions.
+// It has 2 aspects:
+// 1. Typographical: means appearance.
+//------------------------------------------------------------------------------
+//|	Identifier Type		|	Examples										|
+//|	Package				|	com.google.inject, org.joda.time.format			|
+//|	Class or Interface	|	Timer, FutureTask, LinkedHashMap, HttpServlet	|
+//|	Method or Field		|	remove, ensureCapacity, getCrc					|
+//|	Constant Field		|	MIN_VALUE, NEGATIVE_INFINITY					|
+//|	Local Variable		|	i, xref, houseNumber							|
+//|	Type Parameter		|	T, E, K, V, X, T1, T2							|
+//------------------------------------------------------------------------------
+
+// 2. Grammatical
+// Class - noun: Timer, Task
+// Interface - noun, adjective ends with able: Comparator, Comparable
+// Annotation - noun, verb, preposition, adjective: @Test, @Autowired, @ImplementedBy, @ThreadSafe
+// Method - verb, rarely noun: drawImage, getDimension, isInterrupted, size
+// Field - noun: height, capacity
+
+public class CodingConvention {
+
+}
